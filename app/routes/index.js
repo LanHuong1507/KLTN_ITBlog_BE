7630bf11dbffd30eda2b404293d500ccfdb54dce
ThuyTien <vanbac21112k2@gmail.com>
2024-09-20 07:41:18 +0700

    create backend about category,comment

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,19 @@
 const userRoute = require("./user.routes.js");
 const authRoute = require("./auth.routes.js");
-const followerRoute = require("./follower.routes.js");
-const notificationRoute = require("./notification.routes.js");
+const categoryRoute = require("./category.routes.js");
 const articleRoute = require("./article.routes.js");
+const commentRoute = require("./comment.routes.js");
+const notificationRoute = require("./notification.routes.js");
+const followerRoute = require("./follower.routes.js");
+
 function route(app){
+    app.use("/articles", articleRoute);
+    app.use("/comments", commentRoute);
     app.use("/followers", followerRoute);
+    app.use("/notifications", notificationRoute);
+    app.use("/categories", categoryRoute);
     app.use("/users", userRoute);
     app.use("/auth", authRoute);
-    app.use("/notifications", notificationRoute);
-    app.use("/articles", articleRoute);
 }
 
 module.exports = route
